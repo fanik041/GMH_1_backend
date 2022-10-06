a89358591a0140780735047b995bc1855ac900a0
Fahim <fahim.566@gmail.com>
2022-10-06 19:24:22 -0400

    first commit

---
+++ src/main/java/com/example/GMH_Backend_1/GmhBackend1Application.java
@@ -0,0 +1,13 @@
+package com.example.GMH_Backend_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmhBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmhBackend1Application.class, args);
+	}
+
+}
